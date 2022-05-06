3696bde927f393ff9d59194d3a0449cf39d2c0d4
gp-murase <murase@gp1000.jp>
2022-05-06 15:25:02 +0900

    【2. リスト用のテーブル作成】

--- server/src/veujs_tasklist01/src/components/Tutorial.vue
+++ server/src/veujs_tasklist01/src/components/Tutorial.vue
@@ -1,5 +1,20 @@
 <template>
-  <p>test</p>
+  <div>
+    <table>
+      <!-- テーブルヘッダー -->
+      <thead>
+      <tr>
+        <th class="id">ID</th>
+        <th class="comment">コメント</th>
+        <th class="state">状態</th>
+        <th class="button">-</th>
+      </tr>
+      </thead>
+      <tbody>
+      <!-- [1] ここに <tr> で ToDo の要素を1行づつ繰り返し表示したい -->
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
